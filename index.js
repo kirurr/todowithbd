6594d20630f69a829a781edbbf3ffed51bb979a9
kirurr <timon09df@gmail.com>
2023-02-02 20:22:33 +0300

    папка для css, темная темная

--- index.js
+++ index.js
@@ -195,3 +195,42 @@ function success() {
   return false;
   
 };
+
+
+
+
+// theme change
+
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function () { ChangeTheme(); });
+
+function ChangeTheme()
+{
+    let lightTheme = "styles/light.css";
+    let darkTheme = "styles/dark.css";
+
+    var currTheme = link.getAttribute("href");
+    var theme = "";
+
+    if(currTheme == lightTheme)
+    {
+   	 currTheme = darkTheme;
+   	 theme = "dark";
+    }
+    else
+    {    
+   	 currTheme = lightTheme;
+   	 theme = "light";
+    }
+
+    link.setAttribute("href", currTheme);
+
+    Save(theme);
+}
+
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  ChangeTheme();
+}
